2f7ec005d68a7c91ce79605c905545fc2d061012
brush <2829778303@qq.com>
2026-07-09 09:32:19 +0800

    加入了error count

--- day5/locker.v
+++ day5/locker.v
@@ -1,12 +1,30 @@
 module locker_compare(
     input [3:0] scanf,
-    output reg pass
+    output reg pass,
+    input confirm,
+    input [3:0] password,
+    input clk,
+    output reg [1:0] error_count,
+    input rst
 );
-    always @(*) begin
-      if (scanf == password) begin
-        pass = 1;
+    always @(posedge clk) begin
+      if(rst == 1)begin
+        error_count <= 0;
+        pass <= 0;
+      end
+      else begin
+      if (confirm == 1) begin
+        if (scanf == password) begin
+            pass<=1;
+            error_count <= 0;
+        end    
+        else begin
+          pass<=0;
+          error_count<=error_count+1;
+        end 
       end
       else
-        pass = 0;
+        pass <= 0;
+      end
     end
 endmodule
