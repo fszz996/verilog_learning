1051084fb4a40a2c7c820b9fe173a6fd18df0b55
brush <2829778303@qq.com>
2026-07-09 09:59:07 +0800

    添加计时器

---
+++ day5/lock_time.v
@@ -0,0 +1,35 @@
+module lock_time(
+    output reg finish,
+
+    input clk,
+    input rst,
+    input start
+);
+    reg [28:0] count;
+
+    always @(posedge clk)
+    begin
+        if(rst==1)
+        begin
+            count <= 0;
+            finish <= 0;
+        end
+        else if(start==1)
+        begin
+            if(count < 500,000,000)
+            begin
+                count <= count + 1;
+                finish <= 0;
+            end
+            else
+            begin
+                finish <= 1;
+            end
+        end
+        else
+        begin
+            count <= 0;
+            finish <= 0;
+        end
+    end
+endmodule
