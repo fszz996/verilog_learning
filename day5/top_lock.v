f3b8a6cc654982731443bf224f62934203a13545
brush <2829778303@qq.com>
2026-07-09 10:27:34 +0800

    添加顶层连接

---
+++ day5/top_lock.v
@@ -0,0 +1,33 @@
+module top_lock(
+    input clk,
+    input rst,
+    input confirm,
+    input [3:0] scanf,
+    input [3:0] password,
+    output pass,locked,finish
+    );
+
+    wire [1:0] error_count;
+
+    locker_compare ucompare(
+    .clk(clk),
+    .rst(rst),
+    .scanf(scanf),
+    .password(password),
+    .confirm(confirm),
+    .pass(pass),
+    .error_count(error_count)
+    );
+
+    locker_locked ulocked(
+    .error_count(error_count),
+    .locked(locked)
+    );
+
+    lock_time utimer(
+    .clk(clk),
+    .rst(rst),
+    .start(locked),
+    .finish(finish)
+    );
+endmodule
